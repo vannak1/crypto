260266b2fe0b488f8d32d19f4542bc514d9e66f6
Vannak Chhay <vannak@Mac-mini.local>
2018-01-30 13:31:37 -0500

    added example data request

--- App.js
+++ App.js
@@ -1,39 +1,92 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
-
 import React, { Component } from 'react';
 import {
+  NetInfo,
   Platform,
   StyleSheet,
   Text,
+  TouchableOpacity,
   View
 } from 'react-native';
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' +
-    'Cmd+D or shake for dev menu',
-  android: 'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
+export default class App extends Component {
+  pendingSync : undefined 
 
-export default class App extends Component<{}> {
+  componentWillMount() { 
+    this.setState({ 
+      isConnected : undefined,  
+      syncStatus : undefined,  
+      serverResponse : undefined 
+    }); 
+
+    NetInfo.isConnected.fetch().then(isConnected => { 
+      this.setState({isConnected}); 
+    }); 
+    NetInfo.isConnected.addEventListener('connectionChange', this.onConnectedChange); 
+  } 
+  
+  onConnectedChange = (isConnected) => { 
+    var pendingSync = this.pendingSync; 
+    this.setState({isConnected}); 
+    if(pendingSync) { 
+      this.setState({syncStatus : 'Syncing'}); 
+
+      this.submitData(pendingSync).then(() => { 
+        this.setState({syncStatus : 'Sync Complete'}); 
+      }); 
+    } 
+  } 
+
+  submitData(bodyData) { 
+    return fetch('https://posttestserver.com/post.php', { 
+      method : 'POST', 
+      body : JSON.stringify(bodyData) 
+    }).then((response) => { 
+      return response.text(); 
+    }).then((responseText) => { 
+      this.setState({ 
+        serverResponse : responseText 
+      }); 
+    }); 
+  } 
+
+  onSubmitPress = () => { 
+    var isConnected = this.state.isConnected, 
+        submitBody = { 
+          name : 'React Native Cookbook', 
+          timestamp : Date.now() 
+        }; 
+    if(isConnected) { 
+      this.submitData(submitBody); 
+    } else { 
+      this.pendingSync = submitBody; 
+      this.setState({syncStatus : 'Pending'}); 
+    } 
+  } 
+  
   render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit App.js
-        </Text>
-        <Text style={styles.instructions}>
-          {instructions}
-        </Text>
-      </View>
-    );
+    const { 
+      isConnected, 
+      syncStatus, 
+      serverResponse 
+    } = this.state; 
+    return ( 
+      <View style={styles.container}> 
+        <TouchableOpacity onPress={this.onSubmitPress}> 
+          <View style={styles.button}> 
+            <Text style={styles.buttonText}>Submit Data</Text> 
+          </View> 
+        </TouchableOpacity>  
+        <Text style={styles.instructions}> 
+          Connection Status: {isConnected ? 'Connected' : 'Disconnected'} 
+        </Text> 
+        <Text style={styles.instructions}> 
+          Sync Status: {syncStatus} 
+        </Text> 
+        <Text style={styles.instructions}> 
+          Server Response: {serverResponse} 
+        </Text> 
+      </View> 
+    ); 
   }
 }
 
